f50d3c184f1b989013c3707e35ee344f066d0011
KOSTA gabriel <125087394+Kostalaroche1@users.noreply.github.com>
2026-04-27 15:09:09 +0100

    solve recognize face from haba in sign presence

--- src/components/dashboard/Trying.tsx
+++ src/components/dashboard/Trying.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Trying() {
     return (
         <div>
-            {/*  i add this component just for trying how vercel zill react to build team project */}
+            {/*  i add this component just for trying how vercel will react to build team project */}
             HELLO
         </div>
     )
