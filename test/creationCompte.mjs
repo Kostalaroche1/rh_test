57dde40bbeeb15330ab860f3b3be45543ae8ac61
KOSTA gabriel <125087394+Kostalaroche1@users.noreply.github.com>
2026-05-25 11:15:45 +0100

    after get habba modification from github in ajustProvinces with multiple sign présence

--- test/creationCompte.mjs
+++ test/creationCompte.mjs
@@ -5,18 +5,18 @@ import bcrypt from 'bcryptjs'
 const prisma = new PrismaClient()
 
 async function main() {
-   function generateMatricule() {
-  const randomNumber = Math.floor(1000 + Math.random() * 9000); // 4 chiffres
-  const randomLetter = String.fromCharCode(
-    65 + Math.floor(Math.random() * 26)
-  ); // A–Z
-  return `AG-${randomNumber}-${randomLetter}`;
-}
+  function generateMatricule() {
+    const randomNumber = Math.floor(1000 + Math.random() * 9000); // 4 chiffres
+    const randomLetter = String.fromCharCode(
+      65 + Math.floor(Math.random() * 26)
+    ); // A–Z
+    return `AG-${randomNumber}-${randomLetter}`;
+  }
   const hashedPassword = await bcrypt.hash('cria@@123', 10)
   const EtatCivil = {
-    Marie : "Marié(é)",
-    Celibataire : "Celibataire",
-    Divorce : "Divorcé",
+    Marie: "Marié(é)",
+    Celibataire: "Celibataire",
+    Divorce: "Divorcé",
   }
   const Agents = [
     {
@@ -25,10 +25,10 @@ async function main() {
       prenom: 'Saint-jean',
       statut: EtatCivil.Marie,
       dateEntree: new Date('2022-01-01'),
-      datenais : new Date('1970-01-01'),
-      genre : "MASCULIN",
+      datenais: new Date('1970-01-01'),
+      genre: "MASCULIN",
       actif: true,
-      compte:  {
+      compte: {
         login: 'saintJean@gmail.com',
         motDePasse: hashedPassword,
         actif: true,
@@ -40,29 +40,14 @@ async function main() {
       prenom: 'Djota',
       statut: EtatCivil.Celibataire,
       dateEntree: new Date('2022-01-01'),
-      datenais : new Date('1970-01-01'),
-      genre : "MASCULIN",
+      datenais: new Date('1970-01-01'),
+      genre: "MASCULIN",
       actif: true,
-      compte :  {
-      login: 'jonathanmbiya@gmail.com',
-      motDePasse: hashedPassword,
-      actif: true,
-    },
-    },
-    {
-      matricule: generateMatricule(),
-      nom: 'Kosta Blanchard',
-      prenom: 'laroche',
-      statut: EtatCivil.Marie,
-      dateEntree: new Date('2022-01-01'),
-      actif: true,
-      datenais : new Date('1970-01-01'),
-      genre : "MASCULIN",
-      compte :  {
-      login: 'kostablanchard@gmail.com',
-      motDePasse: hashedPassword,
-      actif: true,
-    }
+      compte: {
+        login: 'jonathanmbiya@gmail.com',
+        motDePasse: hashedPassword,
+        actif: true,
+      },
     },
     {
       matricule: generateMatricule(),
@@ -70,14 +55,14 @@ async function main() {
       prenom: 'Mbuyi',
       statut: EtatCivil.Marie,
       dateEntree: new Date('2022-01-01'),
-      datenais : new Date('1970-01-01'),
-      genre : "FEMININ",
+      datenais: new Date('1970-01-01'),
+      genre: "FEMININ",
       actif: true,
-      compte :   {
-      login: 'gracembuyi@gmail.com',
-      motDePasse: hashedPassword,
-      actif: true,
-    },
+      compte: {
+        login: 'gracembuyi@gmail.com',
+        motDePasse: hashedPassword,
+        actif: true,
+      },
     },
   ]
 
@@ -87,17 +72,17 @@ async function main() {
       motDePasse: hashedPassword,
       actif: true,
     },
-     {
+    {
       login: 'jonathanmbiya@gmail.com',
       motDePasse: hashedPassword,
       actif: true,
     },
-     {
+    {
       login: 'gracembuyi@gmail.com',
       motDePasse: hashedPassword,
       actif: true,
     },
-     {
+    {
       login: 'kostablanchard@gmail.com',
       motDePasse: hashedPassword,
       actif: true,
@@ -105,58 +90,57 @@ async function main() {
   ]
   const Role = [
     {
-      key : "admin",
+      key: "admin",
       nom: 'Admin Gen',
       description: "Administrateur Generale il voit tout",
       actif: true,
     },
-     {
-      key : "chefservice",
+    {
+      key: "chefservice",
       nom: 'chef de service',
       description: "chef de service, il voit tout sur son service",
       actif: true,
     },
-     {
-      key : "rh",
+    {
+      key: "rh",
       nom: 'Gestionnaire RH',
       description: "Gestionnaire RH, il s'occupe de la ressources humaines",
       actif: true,
     },
-     {
-      key:"ag",
+    {
+      key: "ag",
       nom: 'Agent',
       description: "Agent , il fait des demandes de congé , rempli les heures d'arrive et est affecté à un service",
       actif: true,
     },
   ]
-const result = []
-   try {
-      const dataAll = await prisma.$transaction(async (db) => {
-        const roles = await db.role.createMany({
-            data : Role
-          })
+  const result = []
+  try {
+    const dataAll = await prisma.$transaction(async (db) => {
+      const roles = await db.role.createMany({
+        data: Role
+      })
 
-        for (const ag of Agents){
-           const agent = await db.agent.create({
-            data: {
-              matricule: ag.matricule,
-              dateEntree: new Date(),
-              nom: ag.nom,
-              genre : ag.genre,
-              datenais : ag.datenais,
-              prenom: ag.prenom,
-              statut: ag.statut,
-              actif: ag.actif,
-            },
+      for (const ag of Agents) {
+        const agent = await db.agent.create({
+          data: {
+            matricule: ag.matricule,
+            dateEntree: new Date(),
+            nom: ag.nom,
+            genre: ag.genre,
+            datenais: ag.datenais,
+            prenom: ag.prenom,
+            statut: ag.statut,
+            actif: ag.actif,
+          },
         })
 
-         if(!agent)
-          {
-            console.log("erreur enregistrement utilisateur")
-            return;
-          }
-        
-        
+        if (!agent) {
+          console.log("erreur enregistrement utilisateur")
+          return;
+        }
+
+
         const utilisateur = await db.utilisateur.create({
           data: {
             login: ag.compte.login,
@@ -164,56 +148,53 @@ const result = []
           },
         })
 
-         if(!utilisateur)
-          {
-            console.log("erreur Id utilisateur compte")
-            return;
-          }
-         const roles = await db.role.findFirst({
-            where : {key : "ag" }
-          })
+        if (!utilisateur) {
+          console.log("erreur Id utilisateur compte")
+          return;
+        }
+        const roles = await db.role.findFirst({
+          where: { key: "ag" }
+        })
 
-          if(!roles)
-          {
-            console.log("erreur Id roles")
-            return;
-          }
-  
+        if (!roles) {
+          console.log("erreur Id roles")
+          return;
+        }
+
         const utilisateurRole = await db.utilisateurRole.create({
           data: {
             roleId: roles.id,
             utilisateurId: utilisateur.id,
-            attribuePar:  utilisateur.id,
+            attribuePar: utilisateur.id,
           },
         })
 
-         if(!agent)
-          {
-            console.log("erreur Id agent")
-            return;
-          }
-  
+        if (!agent) {
+          console.log("erreur Id agent")
+          return;
+        }
+
         const CreationCompte = await db.compteAgent.create({
           data: {
             agentId: agent.id,
             utilisateurId: utilisateur.id,
-            liePar:  utilisateur.id,
+            liePar: utilisateur.id,
           },
         })
-         result.push({ identification : agent, CreationCompte : utilisateur,  attributionRole : utilisateurRole ,Activation :CreationCompte })
-        }
-        return result
-      })
-  
-      console.log(dataAll, 'all data')
-  
-      if (dataAll) {
-         console.log('Compte créé avec succès')
+        result.push({ identification: agent, CreationCompte: utilisateur, attributionRole: utilisateurRole, Activation: CreationCompte })
       }
-      console.log('Compte non créé')
-    } catch (error) {
-      console.error(error) 
+      return result
+    })
+
+    console.log(dataAll, 'all data')
+
+    if (dataAll) {
+      console.log('Compte créé avec succès')
     }
+    console.log('Compte non créé')
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 main()
